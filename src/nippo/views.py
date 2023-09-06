3d68f9d8b7b59d54688d849bc1af218faa5879bf
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-09-06 18:03:18 +0900

    3-1

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from random import randint
+from .models import NippoModel
 
 def nippoListView(request):
-	return render(request, "nippo/nippo-list.html")
+  template_name = "nippo/nippo-list.html"
+  ctx = {}
+  qs = NippoModel.objects.all()
+  ctx["object_list"] = qs
 
-def nippoDetailView(request, number):
-  template_name="nippo/nippo-detail.html"
-  random_int = randint(1,10)
-  ctx = {
-    "random_number": random_int,
-    "number": number,
-  }
+  return render(request, template_name, ctx)
+
+def nippoDetailView(request, pk):
+  template_name = "nippo/nippo-detail.html"
+  ctx = {}
+  q = NippoModel.objects.get(pk=pk)
+  ctx["object"] = q
   return render(request, template_name, ctx)
 
 def nippoCreateView(request):
