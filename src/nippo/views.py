64cb9d4486ea9a698c00790d67dd424530c1e918
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-09-07 17:02:11 +0900

    3-3

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import NippoModel
+from .forms import  NippoFormClass
 
 def nippoListView(request):
   template_name = "nippo/nippo-list.html"
@@ -17,10 +18,26 @@ def nippoDetailView(request, pk):
   return render(request, template_name, ctx)
 
 def nippoCreateView(request):
-    template_name="nippo/nippo-form.html"
-    if request.POST:
-        title = request.POST["title"]
-        content = request.POST["content"]
-        obj = NippoModel(title=title, content=content)
-        obj.save()
-    return render(request, template_name)
+  template_name = "nippo/nippo-form.html"
+  form = NippoFormClass(request.POST or None)
+  ctx = {"form": form}
+  if form.is_valid():
+    title = form.cleaned_data["title"]
+    content = form.cleaned_data["content"]
+    obj = NippoModel(title=title, content=content)
+    obj.save()
+  return render(request, template_name, ctx)
+
+def nippoUpdateFormView(request, pk):
+  template_name = "nippo/nippo-form.html"
+  obj = NippoModel.objects.get(pk=pk)
+  initial_values = {"title": obj.title, "content":obj.content}
+  form = NippoFormClass(request.POST or initial_values)
+  ctx = {"form": form}
+  if form.is_valid():
+    title = form.cleaned_data["title"]
+    content = form.cleaned_data["content"]
+    obj.title = title
+    obj.content = content
+    obj.save()
+  return render(request, template_name, ctx)
